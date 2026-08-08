f1bab275ee2978189e5c0e0c5e01149ea85d8f00
RobIsNotAvailable <robamendolaa@gmail.com>
2026-08-08 22:48:58 +0200

    first commit

---
+++ src/main/java/com/webtech/quicksketch/QuicksketchApplication.java
@@ -0,0 +1,13 @@
+package com.webtech.quicksketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicksketchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicksketchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webtech/quicksketch/QuicksketchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtech.quicksketch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicksketchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
